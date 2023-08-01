578206a1d384ade24f4d30042f0db2025244a989
Raghav chawla <raghchawla.131@gmail.com>
2023-08-01 22:22:15 +0530

    responsive navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primaryTextColor: "#4A1D1F",
+      },
+    },
+  },
+  plugins: [],
+};
+
